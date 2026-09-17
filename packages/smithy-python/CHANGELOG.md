46e628afc651cb520a3cc6bf42c801f9a658f62c
aws-sdk-python-automation <github-aws-sdk-python-automation@amazon.com>
2026-09-17 20:37:06 +0000

    Release 2026-09-17

--- packages/smithy-python/CHANGELOG.md
+++ packages/smithy-python/CHANGELOG.md
@@ -1,1 +1,6 @@
 # Changelog
+
+## v0.1.0
+
+### Features
+* Added the experimental smithy-python package and CLI scaffold.
--- packages/smithy-python/src/smithy_python/__init__.py
+++ packages/smithy-python/src/smithy_python/__init__.py
@@ -2,4 +2,4 @@
 # SPDX-License-Identifier: Apache-2.0
 """A Smithy code generator for Python clients and types."""
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
